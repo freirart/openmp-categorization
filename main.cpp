a3d795a4508261664e0f50ba8f34f52b7f491275
freirart <artur.santos@stone.com.br>
2023-11-18 16:55:44 -0300

    prevents memory burst

--- main.cpp
+++ main.cpp
@@ -152,61 +152,68 @@ void write_final_dataset() {
   final_dataset.open(final_dataset_name);
 
   if (final_dataset.is_open()) {
-    std::string line;
+    while (!concluded_reading_file) {
+      final_dataset_vector.clear();
 
-    printf("> Vou ler o arquivo!\n");
+      printf("Vou começar a ler o arquivo!\n");
 
-    while (getline(dataset_to_read, line)) {
-      std::string content;
-      std::stringstream line_stream(line);
-      std::vector<std::string> row;
+      for (int i = 0; i < MAX_LINES_READ_PER_LOOP; i++) {
+        std::string line;
 
-      while (getline(line_stream, content, ',')) {
-        row.push_back(content);
+        if (getline(dataset_to_read, line)) {
+          std::string content;
+          std::stringstream line_stream(line);
+          std::vector<std::string> row;
+
+          while (getline(line_stream, content, ',')) {
+            row.push_back(content);
+          }
+
+          final_dataset_vector.push_back(row);
+        } else {
+          concluded_reading_file = true;
+          break;
+        }
       }
 
-      final_dataset_vector.push_back(row);
-    }
+      printf("> Vou começar a processar o arquivo!\n");
 
-    printf("> Terminei de ler o arquivo!\n");
-    printf("> Vou começar a processar o arquivo!\n");
-
-    int vec_size = final_dataset_vector.size();
+      int vec_size = final_dataset_vector.size();
 #pragma omp parallel for
-    for (int j = 0; j < NUM_OF_COLS; j++) {
-      auto vec1 = category_indexes;
-      auto raw_index = std::find(vec1.begin(), vec1.end(), j);
-      bool is_categorical_col = raw_index != vec1.end();
-
-      if (is_categorical_col) {
-        for (int i = 1; i < vec_size; i++) {
-          auto content = final_dataset_vector[i][j];
-
-          int index = std::distance(vec1.begin(), raw_index);
-          auto category_name = category_names[index];
-
-          auto vec2 = categorical_dict[category_name];
-          auto raw_index_2 = std::find(vec2.begin(), vec2.end(), content);
-
-          int category_id = std::distance(vec2.begin(), raw_index_2) + 1;
-          final_dataset_vector[i][j] = std::to_string(category_id);
-        }
-      }
-    }
-
-    printf("> Terminei de processar o arquivo!\n");
-    printf("> Vou começar a escrever o arquivo!\n");
-
-    for (int i = 0; i < vec_size; i++) {
       for (int j = 0; j < NUM_OF_COLS; j++) {
-        if (j != 0) {
-          final_dataset << ",";
-        }
+        auto vec1 = category_indexes;
+        auto raw_index = std::find(vec1.begin(), vec1.end(), j);
+        bool is_categorical_col = raw_index != vec1.end();
 
-        final_dataset << final_dataset_vector[i][j];
+        if (is_categorical_col) {
+          for (int i = 1; i < vec_size; i++) {
+            auto content = final_dataset_vector[i][j];
+
+            int index = std::distance(vec1.begin(), raw_index);
+            auto category_name = category_names[index];
+
+            auto vec2 = categorical_dict[category_name];
+            auto raw_index_2 = std::find(vec2.begin(), vec2.end(), content);
+
+            int category_id = std::distance(vec2.begin(), raw_index_2) + 1;
+            final_dataset_vector[i][j] = std::to_string(category_id);
+          }
+        }
       }
 
-      final_dataset << std::endl;
+      printf("> Vou começar a escrever o arquivo!\n");
+
+      for (int i = 0; i < vec_size; i++) {
+        for (int j = 0; j < NUM_OF_COLS; j++) {
+          if (j != 0) {
+            final_dataset << ",";
+          }
+
+          final_dataset << final_dataset_vector[i][j];
+        }
+
+        final_dataset << std::endl;
+      }
     }
 
     printf("> Terminei de escrever o arquivo!\n");
